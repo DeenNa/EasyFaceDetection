d6db5d4891028a84e3bcabad7d3a0f3ec2785e56
DeenNa <deenna_vargilz@yahoo.com>
2012-02-26 04:21:39 +0700

    second commit

---
+++ EasyFaceDetection/DataViewController.h
@@ -0,0 +1,19 @@
+//
+//  DataViewController.h
+//  EasyFaceDetection
+//
+//  Created by Deen Na on 2/18/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+
+-(void)faceDetection;
+-(void)markFaces:(UIImageView *)facePicture;
+
+@end
---
+++ EasyFaceDetection/DataViewController.m
@@ -0,0 +1,171 @@
+//
+//  DataViewController.m
+//  EasyFaceDetection
+//
+//  Created by Deen Na on 2/18/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import "DataViewController.h"
+#import <CoreImage/CoreImage.h>
+#import <QuartzCore/QuartzCore.h>
+
+@implementation DataViewController
+
+@synthesize dataLabel = _dataLabel;
+@synthesize dataObject = _dataObject;
+
+- (void)dealloc
+{
+    [_dataLabel release];
+    [_dataObject release];
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self faceDetection];
+}
+
+-(void)faceDetection {
+    
+    UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"facedetectionpic0.jpg"]];
+    
+    [self.view addSubview:image];
+    
+    //[self markFaces:image];
+    [self performSelector:@selector(markFaces:) withObject:image];
+    
+    //Transfrom ke posisi y-axis dari kooordinat CoreImage
+    [image setTransform:CGAffineTransformMakeScale(1, -1)];
+    
+    //Transfrom View
+    [self.view setTransform:CGAffineTransformMakeScale(1, -1)];
+    
+    [image release];
+    
+}
+
+-(void)markFaces:(UIImageView *)facePicture {
+    
+    CIImage *image = [CIImage imageWithCGImage:facePicture.image.CGImage];
+    
+    CIDetector  *detector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:[NSDictionary dictionaryWithObject:CIDetectorAccuracyHigh forKey:CIDetectorAccuracy]];
+    
+    //Menampung semua komponen wajah/muka yang terdeteksi
+    NSArray     *features = [detector featuresInImage:image];
+    
+    for (CIFaceFeature  *faceFeature in features) {
+        // get the width of the face
+        CGFloat faceWidth = faceFeature.bounds.size.width;
+        
+        // create a UIView using the bounds of the face
+        UIView* faceView = [[UIView alloc] initWithFrame:faceFeature.bounds];
+        
+        // add a border around the newly created UIView
+        faceView.layer.borderWidth = 1;
+        faceView.layer.borderColor = [[UIColor redColor] CGColor];
+        
+        // add the new view to create a box around the face
+        [self.view addSubview:faceView];
+        
+        if(faceFeature.hasLeftEyePosition)
+        {
+            // create a UIView with a size based on the width of the face
+            UIView* leftEyeView = [[UIView alloc] initWithFrame:CGRectMake(faceFeature.leftEyePosition.x-faceWidth*0.15, faceFeature.leftEyePosition.y-faceWidth*0.15, faceWidth*0.3, faceWidth*0.3)];
+            // change the background color of the eye view
+            [leftEyeView setBackgroundColor:[[UIColor blueColor] colorWithAlphaComponent:0.3]];
+            // set the position of the leftEyeView based on the face
+            [leftEyeView setCenter:faceFeature.leftEyePosition];
+            // round the corners
+            leftEyeView.layer.cornerRadius = faceWidth*0.15;
+            // add the view to the window
+            [self.view addSubview:leftEyeView];
+            
+            [leftEyeView release];
+        }
+        
+        if(faceFeature.hasRightEyePosition)
+        {
+            // create a UIView with a size based on the width of the face
+            UIView* leftEye = [[UIView alloc] initWithFrame:CGRectMake(faceFeature.rightEyePosition.x-faceWidth*0.15, faceFeature.rightEyePosition.y-faceWidth*0.15, faceWidth*0.3, faceWidth*0.3)];
+            // change the background color of the eye view
+            [leftEye setBackgroundColor:[[UIColor blueColor] colorWithAlphaComponent:0.3]];
+            // set the position of the rightEyeView based on the face
+            [leftEye setCenter:faceFeature.rightEyePosition];
+            // round the corners
+            leftEye.layer.cornerRadius = faceWidth*0.15;
+            // add the new view to the window
+            [self.view addSubview:leftEye];
+            [leftEye release];
+        }
+        
+        if(faceFeature.hasMouthPosition)
+        {
+            // create a UIView with a size based on the width of the face
+            UIView* mouth = [[UIView alloc] initWithFrame:CGRectMake(faceFeature.mouthPosition.x-faceWidth*0.2, faceFeature.mouthPosition.y-faceWidth*0.2, faceWidth*0.4, faceWidth*0.4)];
+            // change the background color for the mouth to green
+            [mouth setBackgroundColor:[[UIColor greenColor] colorWithAlphaComponent:0.3]];
+            // set the position of the mouthView based on the face
+            [mouth setCenter:faceFeature.mouthPosition];
+            // round the corners
+            mouth.layer.cornerRadius = faceWidth*0.2;
+            // add the new view to the window
+            [self.view addSubview:mouth];
+            
+            [mouth release];
+        }
+        
+    }
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+    
+}
+
+
+
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.dataLabel.text = [self.dataObject description];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
